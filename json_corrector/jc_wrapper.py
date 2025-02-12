70cb1fe6ffb46729b548f615e7570061cd7e7f21
Jannis Pieper <jannis.pieper@yahoo.de>
2025-02-12 22:22:59 +0100

    cleaned up wrapper script and debugging

--- json_corrector/jc_wrapper.py
+++ json_corrector/jc_wrapper.py
@@ -1,20 +1,13 @@
 import os
 import subprocess
 import sys
-from json_corrector import jc
 
 def windows():
     exe_path = os.path.join(os.path.dirname(__file__), 'jc.exe')
-    for i in sys.argv:
-        print(i)
-    subprocess.run([exe_path] + sys.argv[1:])
+    subprocess.run([exe_path])
 
 def linux():
-    #print(sys.argv[1])
-    for i in sys.argv:
-        print(i)
     subprocess.run(["python3", "-m", "json_corrector.jc"])
-    #jc.main(sys.argv[1:])
 
 def main():
     if "linux" in sys.platform:
